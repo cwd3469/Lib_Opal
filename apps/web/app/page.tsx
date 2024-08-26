d8f61f2e5063ba9ef885fe94739cff9c997e843c
choi joo young <cwd3469@naver.com>
2024-08-26 23:13:43 +0900

    test math package 추가

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import { Button } from "@repo/ui/button";
+import { add } from "@repo/math/add";
+
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <div>{add(1, 10)}</div>
       <main className={styles.main}>
         <Image
           className={styles.logo}
