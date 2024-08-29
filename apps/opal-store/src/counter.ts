ac9ec637ca597f1c8b5c635509468fb01dd0f652
jooyoung-choi-bepluslab <cwd3469@beplushealthcare.com>
2024-08-29 17:37:42 +0900

    OPL-2 feat : http module

--- apps/opal-store/src/counter.ts
+++ apps/opal-store/src/counter.ts
@@ -1,9 +1,16 @@
 export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+    let counter = 0;
+    const setCounter = (count: number) => {
+        counter = count;
+        element.innerHTML = `count is ${counter}`;
+    };
+
+    const fetchPost = async () => {
+        await fetch('http://127.0.0.1:3000', {
+            method: 'get',
+        }).then((response) => console.log(response));
+    };
+
+    element.addEventListener('click', fetchPost);
+    setCounter(0);
 }
