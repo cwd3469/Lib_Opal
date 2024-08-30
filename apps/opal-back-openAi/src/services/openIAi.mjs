77fc8eb15de6278c4920864c892d08d528bfd3ba
jooyoung-choi-bepluslab <cwd3469@beplushealthcare.com>
2024-08-30 17:37:57 +0900

    OPL-2 feat : 요약 강의

--- apps/opal-back-openAi/src/services/openIAi.mjs
+++ apps/opal-back-openAi/src/services/openIAi.mjs
@@ -2,24 +2,36 @@ import Dotenv from 'dotenv';
 Dotenv.config();
 import OpenAI from 'openai';
 
+//TODO: env 추가 작업
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
-const response = await openai.chat.completions.create({
-    model: 'gpt-4o',
-    messages: [
-        {
-            role: 'system',
-            content:
-                "You are a Socratic tutor. Use the following principles in responding to students:\n    \n    - Ask thought-provoking, open-ended questions that challenge students' preconceptions and encourage them to engage in deeper reflection and critical thinking.\n    - Facilitate open and respectful dialogue among students, creating an environment where diverse viewpoints are valued and students feel comfortable sharing their ideas.\n    - Actively listen to students' responses, paying careful attention to their underlying thought processes and making a genuine effort to understand their perspectives.\n    - Guide students in their exploration of topics by encouraging them to discover answers independently, rather than providing direct answers, to enhance their reasoning and analytical skills.\n    - Promote critical thinking by encouraging students to question assumptions, evaluate evidence, and consider alternative viewpoints in order to arrive at well-reasoned conclusions.\n    - Demonstrate humility by acknowledging your own limitations and uncertainties, modeling a growth mindset and exemplifying the value of lifelong learning.",
-        },
-        {
-            role: 'user',
-            content: 'Help me to understand the future of artificial intelligence.',
-        },
-    ],
-    temperature: 0.8,
-    max_tokens: 64,
-    top_p: 1,
-});
+/** 추론하기 */
+const get_completion = async (prompt) => {
+    const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: prompt }],
+        temperature: 0,
+    });
+    console.log(response.choices[0].message.content);
+};
+
+const prod_review = `Got this panda plush toy for my daughter's birthday, \
+who loves it and takes it everywhere. It's soft and \ 
+super cute, and its face has a friendly look. It's \ 
+a bit small for what I paid though. I think there \ 
+might be other options that are bigger for the \ 
+same price. It arrived a day earlier than expected, \ 
+so I got to play with it myself before I gave it \ 
+to her.`;
+
+const prompt = `Your task is to generate a short summary of a product \
+review from an ecommerce site. 
+
+Summarize the review below, delimited by triple 
+backticks, in at most 30 words. 
+
+Review: ${prod_review}`;
+
+get_completion(prompt);
