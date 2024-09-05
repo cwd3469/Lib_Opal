8ba7113f988c189a09fae9e1af1515a31be0b14d
jooyoung-choi-bepluslab <cwd3469@beplushealthcare.com>
2024-09-05 14:30:14 +0900

    OPL-3 feat : context 추가

--- packages/i18n/src/locales/en/index.ts
+++ packages/i18n/src/locales/en/index.ts
@@ -1,5 +1,6 @@
 import { default as about } from './about.json';
+import { default as context } from './context.json';
 import { default as font } from './font.json';
 import { default as main } from './main.json';
 import { default as signout } from './signout.json'; 
-export default { about, font, main, signout };
+export default { about, context, font, main, signout };
